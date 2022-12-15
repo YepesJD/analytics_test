f50c8ab0cde4a736ebc7bc4e1f0f2c1ffeada703
David Yepes <david.yepes@conekta.com>
2022-12-15 18:14:59 -0500

    modify home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        0
+        <div className="App1">
+            1
+            <div className="App2">
+                2
+            </div>
+
+            <div className="App3">
+                3
+                <div className="App4">
+                    4
+                </div>
+            </div>
+        </div>
+
+        <div className="App5">
+            5
+            <div className="App6">
+                6
+            </div>
+
+            <div className="App7">
+                7
+                <div className="App8">
+                    8
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
